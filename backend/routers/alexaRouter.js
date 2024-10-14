4aebc35ced1c05edf51bc9e03c304c82b5f68c30
Lin-Xiaowen <linxiaowen2083@outlook.com>
2024-10-13 20:09:41 -0400

    modify the alexa router response structure

--- backend/routers/alexaRouter.js
+++ backend/routers/alexaRouter.js
@@ -3,7 +3,7 @@ const alexaRouter = express.Router();
 
 alexaRouter.post("/command", (req, res) => {
     console.log(JSON.stringify(req.body, null, 2));
-    res.status(200).send("ok");
+    res.status(200).json({ message: "Command received successfully" });
 });
 
 module.exports = alexaRouter;
