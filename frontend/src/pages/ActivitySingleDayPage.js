9965d66085b64dc6eaa9ab0405edeee0399e1d97
Lin-Xiaowen <linxiaowen2083@outlook.com>
2024-10-20 19:00:08 -0400

    redesign the activity page for single day

--- frontend/src/pages/ActivitySingleDayPage.js
+++ frontend/src/pages/ActivitySingleDayPage.js
@@ -2,202 +2,411 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import PageLayoutClean from "../components/PageLayoutClean";
 import "../css/activitySingleDayPage.css";
-import { Col, Row, Typography } from "antd";
-import { blue } from '@ant-design/colors';
+import { Col, Row, Typography, Divider, Card, List, Space } from "antd";
+import {
+  FireOutlined,
+  CheckCircleOutlined,
+  CloseCircleOutlined,
+  ClockCircleOutlined,
+  ThunderboltOutlined,
+  SmileOutlined,
+  FrownOutlined,
+} from "@ant-design/icons";
+import { Pie, Tiny, Column } from "@ant-design/plots";
 
-const { Title } = Typography;
+const { Title, Text } = Typography;
 
 function ActivityPage() {
-  // get username from url
+  // Get username from the URL
   const { username } = useParams();
   const { date } = useParams();
 
-  const data = {
-    activities: [
-      {
-        logId: 8773697742692057000,
-        activityId: 20057,
-        activityParentId: 20057,
-        activityParentName: "Interval Workout",
-        name: "Interval Workout",
-        description: "",
-        calories: 10,
-        distance: 0,
-        steps: 0,
-        duration: 637000,
-        lastModified: "2024-10-18T19:42:31.082Z",
-        startTime: "14:56",
-        isFavorite: false,
-        hasActiveZoneMinutes: true,
-        startDate: "2024-10-18",
-        hasStartTime: true,
-      },
-      {
-        logId: 1899191620171960000,
-        activityId: 3000,
-        activityParentId: 3000,
-        activityParentName: "Workout",
-        name: "Workout",
-        description: "",
-        calories: 27,
-        steps: 12,
-        duration: 1270000,
-        lastModified: "2024-10-18T19:42:31.171Z",
-        startTime: "15:07",
-        isFavorite: false,
-        hasActiveZoneMinutes: true,
-        startDate: "2024-10-18",
-        hasStartTime: true,
-      },
-      {
-        logId: 6367939164943332000,
-        activityId: 90001,
-        activityParentId: 90001,
-        activityParentName: "Bike",
-        name: "Bike",
-        description: "Mountain Biking/BMX",
-        calories: 0,
-        distance: 0,
-        duration: 6000,
-        lastModified: "2024-10-18T19:42:31.082Z",
-        startTime: "15:28",
-        isFavorite: false,
-        hasActiveZoneMinutes: true,
-        startDate: "2024-10-18",
-        hasStartTime: true,
-      },
-      {
-        logId: 5851601774111050000,
-        activityId: 3000,
-        activityParentId: 3000,
-        activityParentName: "Workout",
-        name: "Workout",
-        description: "",
-        calories: 49,
-        steps: 15,
-        duration: 2287000,
-        lastModified: "2024-10-18T20:10:10.626Z",
-        startTime: "15:28",
-        isFavorite: false,
-        hasActiveZoneMinutes: true,
-        startDate: "2024-10-18",
-        hasStartTime: true,
-      },
-      {
-        logId: 116788054595334260,
-        activityId: 90009,
-        activityParentId: 90009,
-        activityParentName: "Run",
-        name: "Run",
-        description: "Running - 5 mph (12 min/mile)",
-        calories: 137,
-        distance: 1.7372,
-        steps: 2537,
-        duration: 1910000,
-        lastModified: "2024-10-18T21:44:46.834Z",
-        startTime: "16:44",
-        isFavorite: false,
-        hasActiveZoneMinutes: true,
-        startDate: "2024-10-18",
-        hasStartTime: true,
-      },
-      {
-        logId: 7577160948127935000,
-        activityId: 90009,
-        activityParentId: 90009,
-        activityParentName: "Run",
-        name: "Run",
-        description: "Running - 5 mph (12 min/mile)",
-        calories: 114,
-        distance: 1.77164,
-        steps: 2330,
-        duration: 1602000,
-        lastModified: "2024-10-18T22:30:23.485Z",
-        startTime: "17:58",
-        isFavorite: false,
-        hasActiveZoneMinutes: true,
-        startDate: "2024-10-18",
-        hasStartTime: true,
-      },
-    ],
-    summary: {
-      caloriesOut: 1656,
-      activityCalories: 373,
-      caloriesBMR: 1288,
-      activeScore: -1,
-      steps: 5351,
-      floors: 3,
-      elevation: 9.144,
-      sedentaryMinutes: 1340,
-      lightlyActiveMinutes: 50,
-      fairlyActiveMinutes: 42,
-      veryActiveMinutes: 8,
-      distances: [
+  const overallActivityEvaluation = 
+  "Today, you burned a total of 1656 calories, including 373 calories from activities. While you completed several workouts and runs, your overall activity level was low, as you spent 1340 minutes sedentary. You logged 5351 steps, which is just over half of your step goal. Your run sessions were the most effective in terms of calorie burn, but activities like biking and interval workouts recorded minimal progress. To meet your goals, consider incorporating more active minutes, focusing on high-calorie-burning activities, and reducing sedentary time.";
+
+
+  const activityData = [
+    {
+      name: "Interval Workout",
+      detail: [
         {
-          activity: "total",
-          distance: 3.7632,
+          title: "Calorie - 10",
+          icon: <FireOutlined style={{ color: "red" }} />,
         },
         {
-          activity: "tracker",
-          distance: 3.7632,
+          title: "Steps - 0",
+          icon: <CheckCircleOutlined style={{ color: "green" }} />,
         },
         {
-          activity: "sedentaryActive",
-          distance: 0.0007,
+          title: "Duration - 10 minutes 37 seconds",
+          icon: <ClockCircleOutlined />,
         },
         {
-          activity: "lightlyActive",
-          distance: 0.5901,
+          title: "Start Time - 14:56",
+          icon: <ClockCircleOutlined />,
         },
         {
-          activity: "moderatelyActive",
-          distance: 2.6458,
-        },
-        {
-          activity: "veryActive",
-          distance: 0.5266,
-        },
-        {
-          activity: "loggedActivities",
-          distance: 3.50884,
-        },
-        {
-          activity: "Interval Workout",
-          distance: 0,
-        },
-        {
-          activity: "Bike",
-          distance: 0,
-        },
-        {
-          activity: "Run",
-          distance: 1.7372,
-        },
-        {
-          activity: "Run",
-          distance: 1.77164,
+          title: "Evaluation - Bad",
+          description:
+            "Your Interval Workout was low in intensity with minimal calorie burn and no recorded steps. Consider increasing the intensity or duration next time for better results.",
+          icon: <FrownOutlined style={{ color: "red" }} />,
         },
       ],
-      marginalCalories: 285,
-      restingHeartRate: 82,
-      heartRateZones: [],
     },
-    goals: {
-      caloriesOut: 2588,
-      steps: 10000,
-      distance: 8.05,
-      floors: 10,
-      activeMinutes: 30,
+    {
+      name: "Workout",
+      detail: [
+        {
+          title: "Calorie - 27",
+          icon: <FireOutlined style={{ color: "red" }} />,
+        },
+        {
+          title: "Steps - 12",
+          icon: <CheckCircleOutlined style={{ color: "green" }} />,
+        },
+        {
+          title: "Duration - 21 minutes 10 seconds",
+          icon: <ClockCircleOutlined />,
+        },
+        {
+          title: "Start Time - 15:07",
+          icon: <ClockCircleOutlined />,
+        },
+        {
+          title: "Evaluation - Average",
+          description:
+            "Your workout was moderate in intensity. Increasing either the duration or intensity could help you achieve better results.",
+          icon: <ThunderboltOutlined style={{ color: "orange" }} />,
+        },
+      ],
     },
+    {
+      name: "Bike",
+      detail: [
+        {
+          title: "Calorie - 0",
+          icon: <FireOutlined style={{ color: "red" }} />,
+        },
+        {
+          title: "Steps - 0",
+          icon: <CloseCircleOutlined style={{ color: "red" }} />,
+        },
+        {
+          title: "Duration - 6 seconds",
+          icon: <ClockCircleOutlined />,
+        },
+        {
+          title: "Start Time - 15:28",
+          icon: <ClockCircleOutlined />,
+        },
+        {
+          title: "Evaluation - Poor",
+          description:
+            "The bike session recorded minimal activity. For better results, aim for a longer, more intense ride next time.",
+          icon: <FrownOutlined style={{ color: "red" }} />,
+        },
+      ],
+    },
+    {
+      name: "Workout",
+      detail: [
+        {
+          title: "Calorie - 49",
+          icon: <FireOutlined style={{ color: "red" }} />,
+        },
+        {
+          title: "Steps - 15",
+          icon: <CheckCircleOutlined style={{ color: "green" }} />,
+        },
+        {
+          title: "Duration - 38 minutes 7 seconds",
+          icon: <ClockCircleOutlined />,
+        },
+        {
+          title: "Start Time - 15:28",
+          icon: <ClockCircleOutlined />,
+        },
+        {
+          title: "Evaluation - Good",
+          description:
+            "Great workout! You burned a good amount of calories and had consistent movement.",
+          icon: <SmileOutlined style={{ color: "green" }} />,
+        },
+      ],
+    },
+  ];
+
+  const singleValueData = [
+    {
+      key: "Basal Metabolic Rate Calories",
+      value: 1288,
+      description:
+        "Your basal metabolic rate is within a typical range. Keep a balanced diet to support this rate.",
+    },
+    {
+      key: "Today's Steps",
+      value: 5351,
+      description:
+        "You've made some progress today, but you're still far from reaching your step goal.",
+    },
+    {
+      key: "Elevation Gained (Meters)",
+      value: 9.144,
+      description:
+        "You have gained a small elevation today. Try increasing activity for more elevation gain.",
+    },
+    {
+      key: "Resting Heart Rate",
+      value: 82,
+      description:
+        "Your resting heart rate is slightly elevated. Consider relaxing and monitoring your heart rate.",
+    },
+  ];
+
+  const activityTimeData = [
+    {
+      type: "Sedentary Minutes",
+      value: 1340,
+    },
+    {
+      type: "Lightly Active Minutes",
+      value: 50,
+    },
+    {
+      type: "Fairly Active Minutes",
+      value: 42,
+    },
+    {
+      type: "Very Active Minutes",
+      value: 8,
+    },
+  ];
+
+  const activityTimeEvaluation =
+    "You have been sedentary for most of the day with minimal active minutes. Consider increasing your activity levels to improve your overall health.";
+
+  const pieConfig = {
+    data: activityTimeData,
+    radius: 1,
+    angleField: "value",
+    colorField: "type",
+    label: {
+      text: "value",
+      position: "outside",
+      style: {
+        fontSize: 13, // Increase font size
+        fontWeight: "bold",
+      },
+    },
+    legend: {
+      color: {
+        title: false,
+        position: "right",
+        rowPadding: 5,
+      },
+    },
+    width: 350, // Set chart width
+    height: 350, // Set chart height
   };
 
-  const summary =
-    "Based on your activity summary, it looks like you’ve had minimal physical activity throughout the day.\nYou burned 793 calories, but with no activity calories recorded, it suggests that you’ve been mostly sedentary.\nYou didn’t take any steps, climb any floors, or cover any distance, and you spent 614 minutes being sedentary.\nAdditionally, there’s no record of light, moderate, or intense physical activity.\nTo improve your overall health, consider gradually increasing your daily physical activity. Start with small goals, like taking short walks to help you work towards your 10,000-step goal or using the stairs to meet your 10-floor goal.\nTry to incorporate at least 30 minutes of moderate exercise each day. Simple activities like stretching or standing up every hour can help reduce sedentary time. Paying attention to your heart rate and tracking your progress over time can also help you stay motivated and improve your active score.";
+  const goalPercentage = [
+    {
+      name: "Steps",
+      goal: 10000,
+      current: 6351,
+      description:
+        "You are making good progress, but there's room to reach your step goal.",
+    },
+    {
+      name: "Floors",
+      goal: 10,
+      current: 2,
+      description:
+        "You have climbed some floors today, but more effort is needed to reach your goal.",
+    },
+    {
+      name: "Distance (km)",
+      goal: 8.05,
+      current: 2.7632,
+      description:
+        "You have covered some distance today, but you are far from the target.",
+    },
+    {
+      name: "Calories Burned",
+      goal: 2588,
+      current: 1656,
+      description:
+        "Good job! You're over halfway to your calorie-burning goal.",
+    },
+    {
+      name: "Active Minutes",
+      goal: 30,
+      current: 100,
+      description:
+        "Amazing! You have surpassed your active minutes goal for the day.",
+    },
+  ];
+
+  const goalPercentageConfigs = [];
+
+  for (let i = 0; i < goalPercentage.length; i++) {
+    let percent = goalPercentage[i].current / goalPercentage[i].goal;
+    let color = "#FF4D4F"; // Default to red
+
+    if (percent >= 1) {
+      percent = 1; // Set to 1 if greater than or equal to 100%
+      color = "#52C41A"; // Green
+    } else if (percent >= 0.5) {
+      color = "#FAAD14"; // Yellow
+    }
+
+    percent = percent.toFixed(1); // Round to one decimal place
+
+    goalPercentageConfigs.push({
+      percent,
+      width: 120,
+      height: 120,
+      color: ["#E8EFF5", color],
+      annotations: [
+        {
+          type: "text",
+          style: {
+            text: `${(percent * 100).toFixed(0)}%`, // Display as integer percentage
+            x: "50%",
+            y: "50%",
+            textAlign: "center",
+            fontSize: 16,
+            fontStyle: "bold",
+          },
+        },
+      ],
+    });
+  }
+
+  const activityCalories = [
+    {
+      name: "Interval Workout",
+      calories: 10,
+    },
+    {
+      name: "Workout (Session 1)",
+      calories: 27,
+    },
+    {
+      name: "Bike",
+      calories: 70,
+    },
+    {
+      name: "Workout (Session 2)",
+      calories: 49,
+    },
+    {
+      name: "Run (Session 1)",
+      calories: 137,
+    },
+    {
+      name: "Run (Session 2)",
+      calories: 114,
+    },
+  ];
+
+  const activityCaloriesConfig = {
+    data: activityCalories,
+    xField: "name",
+    yField: "calories",
+    label: {
+      text: (d) => `${d.calories}`,
+      textBaseline: "bottom",
+    },
+    style: {
+      // Rounded corner style
+      radiusTopLeft: 10,
+      radiusTopRight: 10,
+    },
+    width: 350,
+    height: 350,
+  };
+
+  const activityCaloriesEvaluation =
+    "Today, you burned a total of 337 calories across various activities, including interval workouts, running, and workouts. Your highest calorie-burning activity was running, contributing significantly to your overall calorie expenditure. However, there were some activities like biking that recorded minimal or no calorie burn. ";
 
   return (
     <PageLayoutClean>
+      <Row justify="space-around" align="middle">
+        <Col span={5}>
+          <Title>Activity - {date}</Title>
+        </Col>
+        <Col span={5}>
+          <Title>Welcome, {username}!</Title>
+        </Col>
+      </Row>
       <Row justify="space-around">
-      <Title className="title">h1. Ant Design</Title>
+        <Col span={20}><Text strong>{overallActivityEvaluation}</Text></Col>
+      </Row>
+      <Divider
+        variant="dashed"
+        style={{
+          borderColor: "#2E4265",
+        }}
+      />
+      <Row justify="space-around">
+        <Col span={7}>
+          <Row gutter={[10, 10]}>
+            {activityData.map((activity) => (
+              <Col span={12} key={activity.name}>
+                <Card title={activity.name} bordered={false} size="small">
+                  <List
+                    itemLayout="horizontal"
+                    dataSource={activity.detail}
+                    renderItem={(item) => (
+                      <List.Item>
+                        <List.Item.Meta
+                          avatar={item.icon}
+                          title={<Text strong>{item.title}</Text>}
+                          description={item.description || ""}
+                        />
+                      </List.Item>
+                    )}
+                  />
+                </Card>
+              </Col>
+            ))}
+          </Row>
+        </Col>
+        <Col span={15}>
+          <Row gutter={[10, 10]}>
+            {singleValueData.map((item, index) => (
+              <Col span={5} key={index}>
+                <Card title={item.key} bordered={false} size="small">
+                  Value: {item.value}
+                  <br />
+                  Note: {item.description}
+                </Card>
+              </Col>
+            ))}
+            {goalPercentage.map((item, index) => (
+              <Col span={5} key={index}>
+                <Card title={item.name} bordered={false} size="small">
+                  Goal: {item.goal}; Current: {item.current}
+                  <Tiny.Ring {...goalPercentageConfigs[index]} />
+                  Note: {item.description}
+                </Card>
+              </Col>
+            ))}
+            <Col span={8}>
+              <Card size="small">
+                <Pie {...pieConfig} />
+                <Text strong>{activityTimeEvaluation}</Text>
+              </Card>
+            </Col>
+            <Col span={8}>
+              <Card size="small">
+                <Column {...activityCaloriesConfig} />
+                <Text strong>{activityCaloriesEvaluation}</Text>
+              </Card>
+            </Col>
+          </Row>
+        </Col>
       </Row>
     </PageLayoutClean>
   );
